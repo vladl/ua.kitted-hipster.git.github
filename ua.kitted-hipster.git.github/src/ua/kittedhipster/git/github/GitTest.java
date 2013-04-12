a8792d0d80ca2950f0dc32c0d1e2dff34351f0fb
vladl <vladlen.lychak@gmail.com>
2013-04-12 23:28:46 +0300

    Restored repo

---
+++ ua.kitted-hipster.git.github/src/ua/kittedhipster/git/github/GitTest.java
@@ -0,0 +1,13 @@
+package ua.kittedhipster.git.github;
+
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Git is cool!");
+
+	}
+
+}
